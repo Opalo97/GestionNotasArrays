09085bf1f50ca2fe146ba34f431a885310882b28
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-03-01 12:54:54 +0100

    no acabar

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int opcion;
+        int[] array1 = new int[5];
+        int opcion, suma = 0, promedio = 0, notamayor = 0, notamenor = 0;
         Scanner entrada = new Scanner(System.in);
         do {
             System.out.println("MENÚ DE OPCIONES: ");
@@ -18,7 +19,44 @@ public class Main {
             System.out.println("0.-Salir");
             System.out.print("Elige una opción: ");
             opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    for (int i = 0; i < array1.length; i++) {
+                        System.out.print("Dame la nota número " + (i + 1) + ": ");
+                        array1[i] = entrada.nextInt();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < array1.length; i++) {
+                        System.out.println(array1[i] + " ");
+                    }
+                    break;
+                case 3:
+                    for (int  i = 0; i < array1.length; i++){
+                        suma += array1[i];
+                    }
+                    System.out.println(suma);
+                    break;
+                case 4:
+                    for (int i = 0; i < array1.length; i++) {
+                        promedio = suma / array1[i];
+                    }
+                    System.out.println(promedio);
+                    break;
+                case 5:
+                     for (int i = 0; i < array1.length; i++) {
+                         notamayor = array1[i];
+                     }
+                     System.out.println("La mayor nota es " + notamayor + " y la nota menor es " + notamenor);
+                    break;
+                case 0:
+                    System.out.println("Programa terminado.");
+                    break;
+            }
+
         } while (opcion != 0);
+        System.out.println("La opción seleccionada no es válida");
 
     }
 }
