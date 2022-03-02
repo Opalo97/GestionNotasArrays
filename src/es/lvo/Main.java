f0b93d8b4f7f83ada7f5e83924b33464c3912724
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-03-02 09:25:08 +0100

    acabado

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array1 = new int[5];
-        int opcion, suma = 0, promedio = 0, notamayor = 0, notamenor = 0;
+        int[] notas = new int[5];
+        int opcion, suma, promedio, notamayor, notamenor;
         Scanner entrada = new Scanner(System.in);
         do {
             System.out.println("MENÚ DE OPCIONES: ");
@@ -22,41 +22,55 @@ public class Main {
 
             switch (opcion) {
                 case 1:
-                    for (int i = 0; i < array1.length; i++) {
+                    for (int i = 0; i < notas.length; i++) {
                         System.out.print("Dame la nota número " + (i + 1) + ": ");
-                        array1[i] = entrada.nextInt();
+                        notas[i] = entrada.nextInt();
                     }
                     break;
                 case 2:
-                    for (int i = 0; i < array1.length; i++) {
-                        System.out.println(array1[i] + " ");
+                    for (int i = 0; i < notas.length; i++) {
+                        System.out.print(notas[i] + " ");
+                        System.out.println();
                     }
                     break;
                 case 3:
-                    for (int  i = 0; i < array1.length; i++){
-                        suma += array1[i];
+                    suma = 0;
+                    for (int  i = 0; i < notas.length; i++){
+                        suma += notas[i];
                     }
                     System.out.println(suma);
                     break;
                 case 4:
-                    for (int i = 0; i < array1.length; i++) {
-                        promedio = suma / array1[i];
+                    suma = 0;
+                    for (int i = 0; i < notas.length; i++) {
+                        suma += notas[i];
                     }
+                    promedio = suma / notas.length;
                     System.out.println(promedio);
                     break;
                 case 5:
-                     for (int i = 0; i < array1.length; i++) {
-                         notamayor = array1[i];
+                    notamayor = notas[0];
+                    notamenor = notas[0];
+                     for (int i = 1; i < notas.length; i++) {
+                         if (notas[i] > notamayor) {
+                             notamayor = notas[i];
+                         }
+
+                         if (notas[i] < notamenor) {
+                             notamenor = notas[i];
+                         }
                      }
                      System.out.println("La mayor nota es " + notamayor + " y la nota menor es " + notamenor);
                     break;
                 case 0:
                     System.out.println("Programa terminado.");
                     break;
+                default:
+                    System.out.println("La opción seleccionada no es válida");
+                    break;
             }
-
         } while (opcion != 0);
-        System.out.println("La opción seleccionada no es válida");
 
+        entrada.close();
     }
 }
